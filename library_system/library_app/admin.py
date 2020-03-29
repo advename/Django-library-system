e09bdf784f126f56dd7ab424f4129c801d7daf10
Lars <you@example.com>
2020-03-29 13:50:03 +0200

    Done with days left in dashboard

--- library_system/library_app/admin.py
+++ library_system/library_app/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 
 from .models import Book, BookLoan
 
-# Register your models here.
+# Customize which fields to display in the admin area
+
+
+class BookLoanAdmin(admin.ModelAdmin):
+    readonly_fields = ("loaned_timestamp",)
+    fields = ("user", "book", "loaned_timestamp", "returned_timestamp")
+
+
+# Register your the models with the custom fields display
 admin.site.register(Book)
-admin.site.register(BookLoan)
+admin.site.register(BookLoan, BookLoanAdmin)
--- library_system/library_app/models.py
+++ library_system/library_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+# this is a core python tool, not from django
+from datetime import datetime, timedelta
 
 # Create your models here.
 
@@ -22,3 +24,8 @@ class BookLoan(models.Model):
 
     def __str__(self):
         return f"{self.book.title} - {self.user.username}"
+
+    def daysLeft(self):
+        date_to_return = self.loaned_timestamp + timedelta(days=30)
+        days_left = (date_to_return - self.loaned_timestamp).days
+        return days_left
