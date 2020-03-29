b4ee019555a10c909ee397a796e8c35cf0fd4d22
Lars <you@example.com>
2020-03-29 10:52:03 +0200

    Before implementing is_available

--- library_system/library_app/models.py
+++ library_system/library_app/models.py
@@ -8,6 +8,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     publisher = models.CharField(max_length=200)
+    is_available = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.title} - {self.author}"
@@ -21,4 +22,3 @@ class BookLoan(models.Model):
 
     def __str__(self):
         return f"{self.book.title} - {self.user.username}"
-
--- library_system/library_app/views.py
+++ library_system/library_app/views.py
@@ -4,14 +4,13 @@ from .models import Book, BookLoan
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
-
 @login_required
 def index(request):
     # books_loaned = BookLoan.objects.filter(returned_timestamp__isnull=True)
     # books = Book.objects.all().exclude(bookloan__book=Book.id)
     # https://docs.djangoproject.com/en/3.0/topics/db/queries/#spanning-multi-valued-relationships
-    books = Book.objects.all()
+    # books = Book.objects.all()
+    # books = Book.objects.filter(bookloan__returned_timestamp__isnull=True)
     context = {"books": books}
     return render(request, "library_app/index.html", context)
 
